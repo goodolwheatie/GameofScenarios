7d8c8325c63cb1b4236d43304d345a13788da628
Vincent <loler800@gmail.com>
2017-10-31 00:39:38 -0700

    Now including user registration - beta, need to implement login feature 10/31/2017 0035

--- app/src/main/java/com/example/memeinnovations/gameofscenarios/RegisterActivity.java
+++ app/src/main/java/com/example/memeinnovations/gameofscenarios/RegisterActivity.java
@@ -1,12 +1,27 @@
 package com.example.memeinnovations.gameofscenarios;
 
-import android.app.ActionBar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
 
     private void handleGenderSpinner(){
         // create spinner or dropdown box class
@@ -37,10 +52,66 @@ public class RegisterActivity extends AppCompatActivity {
         ab.setTitle("Register");
         ab.setSubtitle("Create your profile");
     }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Username is required");
+            editTextEmail.requestFocus();
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+        }
+        if (password.isEmpty())
+        {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+        }
+        if (password.length() < 6){
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "User Registration Successful", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Some error has occurred", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void initSignUp() {
+        Button btnReg = (Button) findViewById(R.id.btnRegNow);
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editTextEmail = findViewById(R.id.etCreateEmail);
+        editTextPassword = findViewById(R.id.etCreatePW);
+        progressBar = (ProgressBar) findViewById(R.id.pbRegister);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // initialize buttons
+        initSignUp();
 
         // initialize actionbar title
         actionBarSetup();
