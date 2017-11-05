be4b7080126cf6c6a172939a2057d246f1de0141
Benson Gao <bnsn.gao@gmail.com>
2017-11-05 13:42:18 -0800

    Prelim Game Lobby
    game lobby page creation. popup for rules crashes app but compiles at least

---
+++ app/src/main/java/com/example/memeinnovations/gameofscenarios/GameLobbyActivity.java
@@ -0,0 +1,52 @@
+package com.example.memeinnovations.gameofscenarios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.LinearLayout;
+
+
+/**
+ * Created by Benson Gao on 11/5/2017.
+ */
+
+public class GameLobbyActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_lobby);
+    }
+
+    public void popupRules(View view) {
+
+        // get a reference to the already created layout
+        LinearLayout gameLobby = (LinearLayout) findViewById(R.id.activity_game_lobby);
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.activity_prisoners_dilemma_rules, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupRules = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        popupRules.showAtLocation(gameLobby, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                popupRules.dismiss();
+                return true;            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/memeinnovations/gameofscenarios/MainMenuActivity.java
+++ app/src/main/java/com/example/memeinnovations/gameofscenarios/MainMenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.memeinnovations.gameofscenarios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void play(View view){
+        Intent openGameLobby = new Intent(MainMenuActivity.this, GameLobbyActivity.class);
+        startActivity(openGameLobby);
     }
 }
