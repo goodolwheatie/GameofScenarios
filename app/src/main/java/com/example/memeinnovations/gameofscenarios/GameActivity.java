a5718925a29e45db0a947d21db852a6311813eda
Benson Gao <bnsn.gao@gmail.com>
2017-11-08 18:40:57 -0800

    First Iteration For Game Timer
    crashes for now, i'll fix it later

--- app/src/main/java/com/example/memeinnovations/gameofscenarios/GameActivity.java
+++ app/src/main/java/com/example/memeinnovations/gameofscenarios/GameActivity.java
@@ -1,37 +1,47 @@
 package com.example.memeinnovations.gameofscenarios;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.widget.Chronometer;
+import android.widget.TextView;
 import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
     private static int TIME_OUT = 15000; //Time to launch the another activity
     private boolean betrayed = false;
+    final View activity_game = findViewById(R.id.activity_game);
+    private TextView timer = (TextView)findViewById(R.id.gameTimer);
+    private CountDownTimer gTimer = new CountDownTimer(TIME_OUT* 1000+1000, 1000) {
+
+        public void onTick(long millisUntilFinished) {
+            int seconds = (int) (millisUntilFinished / 1000);
+            timer.setText(String.format("%02d", seconds));
+
+            if(millisUntilFinished < 5000){
+                timer.setTextColor(Color.RED);
+            }
+        }
+
+        public void onFinish() {
+            lockIn(activity_game);
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        final View activity_game = findViewById(R.id.activity_game);
 
-        //gameTimer();
+        gameTimer();
     }
 
-/*    public void gameTimer(){
-
-        new CountDownTimer(TIME_OUT, 1000){
-            public void onTick(long millisUntilFinished){
-                R.id.gameTimer.setText(millisUntilFinished / 1000);
-            }
-
-            public void onFinish(){
-                lockIn(activity_game);
-            }
-        }.start();
-    }*/
+    public void gameTimer(){
+        gTimer.start();
+    }
 
     public void betray(View view){
         betrayed = true;
@@ -43,7 +53,11 @@ public class GameActivity extends AppCompatActivity {
 
     public void lockIn(View view){
         Intent lockIn = new Intent(GameActivity.this, GameFinishActivity.class);
+        lockIn.putExtra("Betrayal",betrayed);
         startActivity(lockIn);
+        if(gTimer!=null){
+            gTimer.cancel();
+        }
         finish();
     }
 
--- app/src/main/java/com/example/memeinnovations/gameofscenarios/GameFinishActivity.java
+++ app/src/main/java/com/example/memeinnovations/gameofscenarios/GameFinishActivity.java
@@ -2,12 +2,24 @@ package com.example.memeinnovations.gameofscenarios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameFinishActivity extends AppCompatActivity {
+
+    private boolean betrayed = false;
+    TextView choice = (TextView)findViewById(R.id.txtChoice);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_finish);
+
+        Bundle bundles= getIntent().getExtras();
+        betrayed = bundles.getBoolean("Betrayal");
+        if(betrayed){
+            choice.setText("You betrayed your partner");
+        }else{
+            choice.setText("You kept quiet");
+        }
     }
 }
