3195b145657684861bbcf9ead822e8416bfa30b1
Benson Gao <bnsn.gao@gmail.com>
2017-12-02 21:02:40 -0800

    Game of Chicken Implementation
    Also included basic reroll functionality (waiting on multiplayer aspects + database stuff for tracking # of reroll tokens to complete it).
    
    Did a little bit of refactoring so GameActivity is now PrisonersActivity along with a few other changes.

---
+++ app/src/main/java/com/example/memeinnovations/gameofscenarios/PrisonersActivity.java
@@ -0,0 +1,96 @@
+package com.example.memeinnovations.gameofscenarios;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ToggleButton;
+import android.widget.TextView;
+import android.view.View;
+
+public class PrisonersActivity extends AppCompatActivity {
+    private static int TIME_OUT = 17000; //Time to launch the another activity
+    private boolean betrayed = false;
+    private View activity_game;
+    private TextView timer;
+    private CountDownTimer gTimer;
+    private ToggleButton btnBetray;
+    private ToggleButton btnKeepQuiet;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prisoners);
+
+        try
+        {
+            //disables the actionBar
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+        activity_game = (ConstraintLayout) findViewById(R.id.activity_game);
+        timer = (TextView) findViewById(R.id.prisonersTimer);
+        //creates and starts the timer for the game
+        gTimer = new CountDownTimer(TIME_OUT, 1000) {
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished / 1000);
+                seconds--;
+                timer.setText(String.format("%02d", seconds));
+                if (seconds < 6) {
+                    timer.setTextColor(Color.RED);
+                }
+            }
+            public void onFinish() {
+                lockIn(activity_game);
+            }
+        };
+        gameTimer();
+
+        //have the game initialize with the keeping quiet option
+        btnBetray = (ToggleButton) findViewById(R.id.btnBetray);
+        btnKeepQuiet = (ToggleButton) findViewById(R.id.btnKeepQuiet);
+        keepQuiet(findViewById(R.id.activity_game));
+    }
+
+    public void gameTimer() {
+        gTimer.start();
+    }
+
+    public void betray(View view) {
+        betrayed = true;
+        //check this button
+        btnBetray.setChecked(true);
+        //uncheck other button
+        btnKeepQuiet.setChecked(false);
+    }
+
+    public void keepQuiet(View view) {
+        betrayed = false;
+        //check this button
+        btnKeepQuiet.setChecked(true);
+        //uncheck other button
+        btnBetray.setChecked(false);
+    }
+
+    public void lockIn(View view) {
+        Intent lockIn = new Intent(PrisonersActivity.this, GameFinishActivity.class);
+        lockIn.putExtra("gameName", "prisoners"); //send the game name
+        lockIn.putExtra("Betrayal", betrayed); //send the decision made to the next activity
+        startActivity(lockIn);
+        if (gTimer != null) {
+            //close the timer
+            gTimer.cancel();
+        }
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //disables the back button in-game
+    }
+
+}
