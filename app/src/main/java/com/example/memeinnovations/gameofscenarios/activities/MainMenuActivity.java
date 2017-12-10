743ef812c9e4b75932b9f5a728b62c96861e07a1
Benson Gao <bnsn.gao@gmail.com>
2017-12-10 00:05:57 -0800

    Hamburger Menu
    Hamburger Menu Functionality

--- app/src/main/java/com/example/memeinnovations/gameofscenarios/activities/MainMenuActivity.java
+++ app/src/main/java/com/example/memeinnovations/gameofscenarios/activities/MainMenuActivity.java
@@ -1,13 +1,22 @@
 package com.example.memeinnovations.gameofscenarios.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.memeinnovations.gameofscenarios.data.FirebaseDB;
 import com.example.memeinnovations.gameofscenarios.R;
@@ -19,8 +28,13 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainMenuActivity extends AppCompatActivity {
 
     private boolean anonymousUser = false;
-    Button btnProfileAndReg;
-    TextView tvwSignOut;
+    private Button btnProfileAndReg;
+    private TextView tvwSignOut;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +56,94 @@ public class MainMenuActivity extends AppCompatActivity {
                 btnProfileAndReg.setText(R.string.register_now);
             }
         }
+
+        // initialize sidebar list
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.mainMenu);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        addDrawerItems();
+        setupDrawer();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("Main Menu");
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = {"",""};
+        if(anonymousUser){
+            osArray[0] = "Register";
+            osArray[1] = "Delete Temp Profile";
+        }else{
+            osArray[0] = "Profile";
+            osArray[1] = "Sign Out";
+        }
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch((int) id){
+                    case 0:
+                    {
+                        profile(findViewById(R.id.mainMenu));
+                        break;
+                    }
+                    case 1:
+                    {
+                        signOut(findViewById(R.id.mainMenu));
+                        break;
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void play(View view) {
